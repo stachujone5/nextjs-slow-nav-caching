106bfce6c460353c68eeff86c44c37795810a0bf
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2024-03-22 15:16:23 +0100

    x

--- src/app/dynamic/page.tsx
+++ src/app/dynamic/page.tsx
@@ -1,17 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 const demoCall = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/");
 
-  return "...";
+  return data.json();
 };
 
 export default async function DynamicPage() {
   const data = await demoCall();
 
-  console.log(data);
+  cookies();
+
+  console.log(JSON.stringify(data));
 
   return (
     <div className="container bg-background rounded-md p-4 flex items-center gap-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 
 const demoCall = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/");
 
-  return "...";
+  return data.json();
 };
 
 export default async function Home() {
   const data = await demoCall();
 
   cookies();
-  console.log(data);
+  console.log(JSON.stringify(data));
 
   return (
     <div className="container bg-background rounded-md p-4 flex items-center gap-2">
